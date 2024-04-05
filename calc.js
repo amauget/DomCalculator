5b36921d77c1a4bed4d66a10ecd41f114bdb0a4f
AaronMauget <amauget@hotmail.com>
2024-04-04 18:37:57 -0700

    initiate calc if more than one operator is entered allow - exception after * or / replace multiple operator entry convert single entry to number when = is pressed. use numtype to prevent chars added to prev answer return null for = event if final index is operator
    begin linking buttons to keys

--- calc.js
+++ calc.js
@@ -11,46 +11,72 @@ const append = document.querySelectorAll('.append');
 const operator = document.querySelectorAll('.operator')
 
 let numberArray = [];
+
+// alert("Welcome! The minus key also functions for negative values")
+keyToButton();
 eventListeners();
 
+function keyToButton(){
+    document.addEventListener('keydown',(event) => {
+        if(event.key >= 0 && event.key <= 9){
+            console.log(event.key)
+        }
+    })
+}
+
 function eventListeners(){
-    //numerical/operator button values:
-    
     append.forEach(button =>{
+        //DEBUGGING NOTE: ADD CONDITIONAL TO HANDLE TEXT INPUTS WHEN A PREVIOUS ANSWER IS PRESENT.
         button.addEventListener('click',() => {
-            let number = button.textContent;
-          
-            numberArray.push(number)
-            output.textContent = numberArray.join('');
-            //Outputs the most recent push due to index = length -1
+            if(typeof(numberArray[numberArray.length -1]) === 'number'){
+                return null;
+            }
+            else{
+                let number = button.textContent;
+                numberArray.push(number)
+                output.textContent = numberArray.join('');
+                //Outputs the most recent push due to index = length -1
+            }
             return numberArray;
-       
         })
     })
     operator.forEach(button =>{
         button.addEventListener('click', () =>{
-            const multDivide = ['*','÷']
-
+            const allOperators = ['+','-','*','÷'] 
+            const multDiv = ['*','÷']
+            const plusMultDiv = ['+','*','÷']
+            const minusPlus = ['-','+']
             let operator = button.textContent
-            
-            if((operator !== '-' && numberArray.length === 0)||(multDivide.includes(numberArray[numberArray.length -1]) && operator !== '-')){
+            console.log(numberArray.length-1)
+            if((operator === numberArray[numberArray.length-1]) || (minusPlus.includes(numberArray[numberArray.length-1]) && allOperators.includes(operator))){
+                
+               numberArray.pop()
+               numberArray.push(operator)
+                output.textContent = numberArray.join('')
+                return numberArray
+                
+            }
+            if((operator !== '-' && numberArray.length === 0)||(multDiv.includes(numberArray[numberArray.length -1]) && operator !== '-') || (operator === '-' && minusPlus.includes(numberArray[numberArray.length -1]) )){
             // if operation doesn't start with '-'              if previous entry to operation is '*' or '÷' and current operation entry is other than '-'
                 return null
             }
-            
+            for(let i = 1; i <= numberArray.length; i++){
+                if((plusMultDiv.includes(numberArray[i]) && operator !== '-')|| (minusPlus.includes(numberArray[i]))){
+                    calculate(numberArray, output)
+                    output.textContent += operator;
+                    console.log('if triggered')
+                    return numberArray.push(...operator)
+                }
+            }
             output.textContent += operator;
             return numberArray.push(...operator)
-            
        })
-        
     })
     equal.addEventListener('click', () => {
-        const allOperators = ['+','-','*','÷'] //subtraction removed to meet negative conditions
+        const allOperators = ['+','-','*','÷'] 
         let negNum = null;
         let restOfArray = null;
         for(let i = 1; i <= numberArray.length; i++){
-            console.log(numberArray[i])
-            
             if(allOperators.includes(numberArray[i])){
                 if(numberArray[0] === '-'){
                     console.log(numberArray[i])
@@ -58,44 +84,40 @@ function eventListeners(){
                     restOfArray = numberArray.slice(i)
                     numberArray.length = 0;
                     numberArray.splice(0,0,negNum, ...restOfArray)
-                    console.log('first condition')
-                    console.log('negNum: ' + negNum)
-                    console.log('restOfArray: ' + restOfArray)
+                    
                 }
             }
         }
-
         for(let i = 0; i <= allOperators.length; i++){
+            //condition had to search allOperators instead. It was triggering in prior loop before numberArray[i] could reach operator.
             if(!numberArray.includes(allOperators[i]) && numberArray[0] === '-'){
                 negNum = parseFloat(numberArray.slice(1,).join('')) * -1
                 numberArray.length = 0;
                 numberArray.push(negNum);
-                console.log('second condition')
-                console.log(negNum)
+                
         }
-        partition(numberArray, output)}
-    
+        calculate(numberArray, output)}
     })
-
     clear.addEventListener('click',() =>{
         numberArray.length = 0;
         output.textContent = ''
     })
-
     backspace.addEventListener('click',() => {
         numberArray.pop()
         //removes last element of array, except for previous answer which deletes entirely.
         output.textContent = numberArray.join('');
-        
     })
 }
-
-
-function partition(userInput, output){
-    // console.log(userInput)
+function calculate(userInput, output){
+    console.log(userInput)
+    if(userInput.length === 1){
+        let answer = parseFloat(userInput[0])
+        userInput.length = 0;
+        return userInput.push(answer) //push leaves Number dataType intact
+        //number conversion utilized to identify what has passed through calculate function
+    }
     let operators = ['+','-','*','÷'];
     let op = ''
-
     for (o in operators){
         for(i in userInput){
             if (userInput[i] === operators[o]){
@@ -106,17 +128,17 @@ function partition(userInput, output){
     let a = [];
     let b = [];
 
-    for(i in userInput){
+    for(let i =0; i < userInput.length; i++){
         if(userInput[i] === op){
-            let position = (parseInt(i));
+            let position = (parseFloat(i));
             a = ((userInput.slice(0,position)).join()).replaceAll(',','');
             //In order: slice at index of 0 through i, join into string, remove all commas, and convert to int.
-            
+
             b = (userInput.slice(position + 1)).join().replaceAll(',','');
             //b adds 1 to position to omit operator.
             a = decimal(a), b = decimal(b)
-            
             //adds contition to handle decimal entry without a leading zero
+
             function decimal(num){
                 if(num[0]=== '.'){
                     num = num.replace('.','0.')
@@ -136,7 +158,6 @@ function partition(userInput, output){
         "÷": (a,b) => a / b
       };
     if(op in this.methods){
-        // output.textContent =
         let answer = this.methods[op](a,b);
         userInput.length = 0;
         output.textContent = answer;
